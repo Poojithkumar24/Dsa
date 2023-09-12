8fc135c92dee776a3095be964bbc15b947a37b0b
Poojith Kumar <109473206+Poojithkumar24@users.noreply.github.com>
2023-09-12 10:08:59 +0530

    Time: 2 ms (97.48%) | Memory: 44.2 MB (96.23%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
@@ -1,36 +1,13 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int max_dis=0;
-
-        for(int i=0;i<seats.length;i++){
-            if(seats[i]==0){
-                max_dis=Math.max(max_dis,f(seats,i));
+        int res = 0, n = seats.length, last = -1;
+        for (int i = 0; i < n; ++i) {
+            if (seats[i] == 1) {
+                res = last ==-1 ? i : Math.max(res, (i - last) / 2);
+                last = i;
             }
         }
-
-        return max_dis;
-    }
-
-    int f(int[] seats,int ind){
-        int left=ind;
-        int right=ind;
-
-        for(int i=ind-1;i>=0;i--){
-            if(seats[i]==1){
-                left=i;
-                break;
-            }
-        }
-        for(int i=ind+1;i<seats.length;i++){
-            if(seats[i]==1){
-                right=i;
-                break;
-            }
-        }
-
-        if(ind==0) return right;
-        if(ind==seats.length-1) return ind-left;
-
-        return Math.min(right-ind,ind-left);
+        res = Math.max(res, n - last - 1);
+        return res;
     }
 }
